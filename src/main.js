17d5c5cfe1c119364f9a320719997b918d5113fb
ecruz17 <ecruz17@ucol.mx>
2020-12-10 10:56:56 -0600

    implemented first function and tested on npm

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero) {
+
+        let total = 0;
+
+        for(let i = 1; i <= numero; i++) {
+            total = total + (1/i);
+        }
+
+        return total;
+    }
+
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(7))
